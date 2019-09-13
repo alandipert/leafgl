ab2afaed3c58913499425d4d5eea2232b0b03b79
Alan Dipert <alan@dipert.org>
2019-09-13 06:33:25 +0000

    color wip

--- R/glify-polygons.R
+++ R/glify-polygons.R
@@ -30,6 +30,7 @@
 #' @export addGlPolygons
 addGlPolygons <- function(map, lng = NULL, lat = NULL, layerId = NULL, group = NULL,
                           popup = NULL,
+                          color = NULL,
                           data = leaflet::getMapData(map)) {
   pgons <- derivePolygons(data, lng, lat, missing(lng), missing(lat), "addGlPolygons")
   # Need to write derivePolygons -> geojson converter
@@ -37,7 +38,7 @@ addGlPolygons <- function(map, lng = NULL, lat = NULL, layerId = NULL, group = N
   # If we fast-track sf objects, at least reduce them to lat/lng before converting
   #geojson <- polygonsGeoJSON(pgons)
   # Only reason passing data: if they are formula, then evaluated in context of data
-  leaflet::invokeMethod(map, data, 'addGlifyPolygons', jsonify::to_json(pgons, unbox = TRUE, by = "column"), NULL, popup, NULL, group) %>%
+  leaflet::invokeMethod(map, data, 'addGlifyPolygons', jsonify::to_json(pgons, unbox = TRUE, by = "column"), color, popup, NULL, group) %>%
     leaflet::expandLimitsBbox(pgons)
   # popup argument just needs to be passed to client
 }
--- inst/htmlwidgets/Leaflet.glify/addGlifyPolygons.js
+++ inst/htmlwidgets/Leaflet.glify/addGlifyPolygons.js
@@ -19,8 +19,16 @@
     };
   }
 
-  LeafletWidget.methods.addGlifyPolygons = function(data, cols, popup, opacity, group) {
-    console.log(popup)
+  function hexToRgb(hex) {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex.toLowerCase());
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
+  }
+
+  LeafletWidget.methods.addGlifyPolygons = function(data, color, popup, opacity, group) {
     // popup argument should be the same length as data
     // popup = null, same # elements, some other length
     // shorter = recycle (helper functions in JS)
@@ -29,7 +37,13 @@
         map: map,
         data: toGeoJSON(data),
         className: group,
+        color: (idx, feature) => {
+          if (color !== null) {
+            return hexToRgb(color[feature.properties.index]);
+          }
+        },
         click: (e, feature) => {
+          console.log(e, feature)
           if (map.hasLayer(shapeslayer.glLayer) && popup !== null) {
             L.popup()
               .setLatLng(e.latlng)
